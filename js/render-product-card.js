507716f313a6184c3b9c609796d99962d13f5401
Evgeny Dudarev <evgduda@mail.ru>
2019-12-26 12:31:22 +0300

    Designed js-function for rendering product-card-tempalte

--- js/render-product-card.js
+++ js/render-product-card.js
@@ -1,5 +1,7 @@
+//Простой шаблон карточки продукта, основанный на интерполяции строк
+
 export let renderProductCard = productData => {
-  return `<div class="popup-container">
+  let productCardMainTemplate = `<div class="popup-container">
   <div class="popup-container__button-close">
     <button class="button-close-icon button-close">Закрыть</button>
   </div>
@@ -7,101 +9,56 @@ export let renderProductCard = productData => {
     <div class="product-card__image-container product-card__image-container_popup">
       <img
         class="product-card__image"
-        src="images/${productData.image}"
-        alt="${productData.name}"
-      />
-       ${
-         productData.salePrice
-           ? '<div class="product-card__sale">sale</div>'
-           : ""
-       }; 
+        src="/${productData.image}"
+        alt="${productData.name}"/>
+       
+        ${
+          productData.salePrice
+            ? '<div class="product-card__sale">sale</div>'
+            : ""
+        };
     </div>
 
     <div class="product-card__name">${productData.name}</div>
+
     ${
       productData.salePrice
-        ? `<div class="product-card__price"> <div class="product-card__old-price">${productData.price} ₽</div>${productData.salePrice}  ₽</div>`
-        : `<div class="product-card__price">
-    ${productData.price} ₽</div>`
-    };
+        ? `<div class="product-card__price"> <div class="product-card__old-price">${productData.price} ₽</div>${productData.salePrice}  ₽</div>\n`
+        : `<div class="product-card__price"> ${productData.price} ₽</div>\n`
+    } 
 
-    <h4 class="product-card__description">
-    ${productData.description}
-    </h4>
+    <h4 class="product-card__description">${productData.description}</h4>\n`;
 
-</div>`;
+  let submitButtonTemplate =
+    '<input class="button-submit product-card__button-submit" type="submit" value="Заказать"></input>';
+
+  if (!productData.sizes) {
+    //если у товар безразмерный, то возвращаем шаблон
+    return productCardMainTemplate + "</div>\n" + submitButtonTemplate;
+  } else {
+    //иначе дополнительно генерируем для каждого размера блоки input-label с учётом доступности размера
+    let productCardSizesTemplate = '<div class="product-card__sizes">\n';
+
+    for (let size in productData.sizes) {
+      let labelPart = "";
+      let inputPart = "";
+
+      if (productData.sizes[size] == false) {
+        labelPart = `<label class="radio-button-size-item__label label_disabled" for="${size}">${size}</label>\n`;
+        inputPart = `<input class="radio-button-size-item" type="radio" name="size" value="${size}" id="${size} disabled"></input>\n`;
+      } else {
+        labelPart = `<label class="radio-button-size-item__label" for="${size}">${size}</label>\n`;
+        inputPart = `<input class="radio-button-size-item" type="radio" name="size" value="${size}" id="${size}"></input>\n`;
+      }
+      productCardSizesTemplate += inputPart + labelPart;
+    }
+
+    return (
+      productCardMainTemplate +
+      productCardSizesTemplate +
+      "</div>\n" +
+      "</div>\n" +
+      submitButtonTemplate
+    );
+  }
 };
-/* 
-<div class="product-card__sizes">
-<input
-  class="radio-button-size-item "
-  type="radio"
-  name="size"
-  value="xs"
-  id="xs-2"
-></input>
-<label class="radio-button-size-item__label" for="xs-2">
-  xs
-</label>
-
-<input
-  class="radio-button-size-item "
-  type="radio"
-  name="size"
-  value="s"
-  id="s-2"
-></input>
-<label class="radio-button-size-item__label" for="s-2">
-  s
-</label>
-
-<input
-  class="radio-button-size-item "
-  type="radio"
-  name="size"
-  value="m"
-  id="m-2"
-></input>
-<label class="radio-button-size-item__label" for="m-2">
-  m
-</label>
-
-<input
-  class="radio-button-size-item "
-  type="radio"
-  name="size"
-  value="l"
-  id="l-2"
-></input>
-<label class="radio-button-size-item__label" for="l-2">
-  l
-</label>
-
-<input
-  class="radio-button-size-item "
-  type="radio"
-  name="size"
-  value="xl"
-  id="xl-2"
-></input>
-<label class="radio-button-size-item__label" for="xl-2">
-  xl
-</label>
-
-<input
-  class="radio-button-size-item "
-  type="radio"
-  name="size"
-  value="xxl"
-  id="xxl-2"
-></input>
-<label class="radio-button-size-item__label" for="xxl-2">
-  xxl
-</label>
-</div>
-</div>
-<input
-class="button-submit product-card__button-submit"
-type="submit"
-value="Заказать"
-></input> */
