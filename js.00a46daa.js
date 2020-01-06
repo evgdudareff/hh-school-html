39dca7a93806d698649ade8cf473e7e6bb03357c
Evgeny Dudarev <evgduda@mail.ru>
2020-01-06 11:52:44 +0300

    Updates

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -2106,7 +2106,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63901" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64323" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
